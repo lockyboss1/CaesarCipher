52c392c5ed3cbcde9aae5235f553e6f0ba9b013c
Ayomide <sonugaengineer@gmail.com>
2020-09-28 12:18:31 +0100

    v1.2

--- CaesarCipher/wwwroot/js/decryptText.js
+++ CaesarCipher/wwwroot/js/decryptText.js
@@ -1,57 +1,82 @@
-﻿let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+﻿
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 let encryptButton = document.getElementById("myButton");
-let encryptText = document.getElementById("encrypt-text").value;
+let encryptText;
 let secretMessage;
 let encryptedMessage = [];
-let encryptNumber = document.getElementById("encrypt-number").value;
 let encryptedText = document.getElementById("encrypted-text");
+let myNumber;
+let encryptNumber;
+let selection = document.querySelector('select');
+let clear = document.getElementById('clear');
+let upload = document.getElementById('upload');
 
-encryptButton.addEventListener("click", function () {
+//gets the the message to be encrypted from the textarea   
+function getText() {
+    encryptText = document.getElementById("encrypt-text").value;
+}
+
+//allows the user to input a local text file to be encrypted
+upload.addEventListener("change", function () {
+    if (this.files && this.files[0]) {
+        var myFile = this.files[0];
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function (e) {
+            encryptText = document.getElementById("encrypt-text").value;
+            let showFileText = document.getElementById("encrypt-text");
+            encryptText = e.target.result;
+            showFileText.textContent = e.target.result;
+        });
+
+        reader.readAsBinaryString(myFile);
+    }
+});
+
+//gets the value from the the dropdown to be used as the encyrption number 
+selection.addEventListener('change', () => {
+    myNumber = selection.options[selection.selectedIndex].text;
+    encryptNumber = parseInt(myNumber);
+    console.log(encryptNumber);
+});
+
+//clears the textboxes and the decrypted message array when pressed
+clear.addEventListener('click', () => {
+    encryptedText.innerHTML = "";
+    let showFileText = document.getElementById("encrypt-text");
+    showFileText.value = "";
+    showFileText.innerHTML = "";
+    encryptedMessage = [];
+});
+
+function caesarDecrypt() {
     var areEqual = encryptText.toLowerCase();
     secretMessage = areEqual.split('');
-    console.log(secretMessage);
 
     for (let i = 0; i <= secretMessage.length; i++) {
         let letterPosition = secretMessage[i];
         let alphabetPosition = alphabet.indexOf(letterPosition);
 
+        //if the position of the alphabet is less than 0, do nothing
         if (alphabetPosition < 0) {
-            let letterPositionMinusThree = (alphabetPosition + 0);
-            let newEncryptionMessage = alphabet[letterPositionMinusThree];
+            let letterPositionPlusThree = (alphabetPosition + 0);
+            let newEncryptionMessage = alphabet[letterPositionPlusThree];
             encryptedMessage.push(newEncryptionMessage);
             let output = encryptedMessage.join(" ");
-            console.log(output);
             encryptedText.innerHTML = output;
         }
 
-        else if (alphabetPosition == 0) {
-            let letterPositionMinusThree = 26 - 3;
-            let newEncryptionMessage = alphabet[letterPositionMinusThree];
-            encryptedMessage.push(newEncryptionMessage);
-            let output = encryptedMessage.join("  ");
-        }
-
-        else if (alphabetPosition == 1) {
-            let letterPositionMinusThree = 27 - 3;
-            let newEncryptionMessage = alphabet[letterPositionMinusThree];
-            encryptedMessage.push(newEncryptionMessage);
-            let output = encryptedMessage.join("  ");
-        }
-
-        else if (alphabetPosition == 2) {
-            let letterPositionMinusThree = 28 - 3;
-            let newEncryptionMessage = alphabet[letterPositionMinusThree];
-            encryptedMessage.push(newEncryptionMessage);
-            let output = encryptedMessage.join("  ");
-        }
-
+        //if it's not less than 0, perform the encrytion operation 
         else {
-            let letterPositionMinusThree = (alphabetPosition - 3);
-            let newEncryptionMessage = alphabet[letterPositionMinusThree];
+            let letterPositionPlusThree = ((alphabetPosition - encryptNumber) + 26) % 26;
+            console.log(encryptNumber);
+            let newEncryptionMessage = alphabet[letterPositionPlusThree];
             encryptedMessage.push(newEncryptionMessage);
-            let output = encryptedMessage.join("  ");
+            let output = encryptedMessage.join("");
         }
     }
-})
+}
+
+encryptButton.addEventListener('click', caesarDecrypt);
--- CaesarCipher/wwwroot/js/encryptText.js
+++ CaesarCipher/wwwroot/js/encryptText.js
@@ -18,6 +18,23 @@ function getText() {
     encryptText = document.getElementById("encrypt-text").value;
 }
 
+//allows the user to input a local text file to be encrypted
+upload.addEventListener("change", function () {
+    if (this.files && this.files[0]) {
+        var myFile = this.files[0];
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function (e) {
+            encryptText = document.getElementById("encrypt-text").value;
+            let showFileText = document.getElementById("encrypt-text");
+            encryptText = e.target.result;
+            showFileText.textContent = e.target.result;
+        });
+
+        reader.readAsBinaryString(myFile);
+    }
+});
+
 //gets the value from the the dropdown to be used as the encyrption number 
 selection.addEventListener('change', () => {
     myNumber = selection.options[selection.selectedIndex].text;
@@ -28,15 +45,15 @@ selection.addEventListener('change', () => {
 //clears the textboxes and the decrypted message array when pressed
 clear.addEventListener('click', () => {
     encryptedText.innerHTML = "";
-   // encryptText = "";
-    //encryptText.innerHTML = "";
+    let showFileText = document.getElementById("encrypt-text");
+    showFileText.value = "";
+    showFileText.innerHTML = "";
     encryptedMessage = [];
 });
 
 function caesarEncrypt() {
     var areEqual = encryptText.toLowerCase();
     secretMessage = areEqual.split('');
-    console.log(secretMessage);
 
     for (let i = 0; i <= secretMessage.length; i++) {
         let letterPosition = secretMessage[i];
@@ -47,8 +64,7 @@ function caesarEncrypt() {
             let letterPositionPlusThree = (alphabetPosition + 0);
             let newEncryptionMessage = alphabet[letterPositionPlusThree];
             encryptedMessage.push(newEncryptionMessage);
-            let output = encryptedMessage.join("  ");
-            console.log(output);
+            let output = encryptedMessage.join(" ");
             encryptedText.innerHTML = output;
         }
 
